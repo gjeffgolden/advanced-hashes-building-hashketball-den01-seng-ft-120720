8e7b81a98be1cfaa32a3eff4532fbf2af66acd8c
Jeff Golden <gjgolden08@gmail.com>
2020-11-28 18:43:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash
   {:home => 
-    {:team_name => " "}
-      {:colors => [" ", " "],
+    {:team_name => "Brooklyn Nets"}
+      {:colors => ["Black", "White"],
       {:players => [
         {:player_name => " " },
         {:number => Integer},
