a454f44781f00a9c5c8b05c858e8f2963ec8482c
Jeff Golden <gjgolden08@gmail.com>
2020-11-28 18:42:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,14 @@ def game_hash
   {:home => 
     {:team_name => " "}
       {:colors => [" ", " "],
-    {:players => [{},{}]
+      {:players => [
+        {:player_name => " " },
+        {:number => Integer},
+        {:shoe => Integer},
+        {:points => Integer},
+        
+        {
+      ]
   },
   {:away => {}}
   
